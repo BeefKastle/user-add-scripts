79c0f8c5d53872829f2dd68ce63adff23d8b0050
Andrew Slejzer <brokensound548@gmail.com>
2019-09-24 15:19:51 -0400

    Inital commit

---
+++ adddev.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#set directory to get names from, set group to add names to, and specify skel dir.
+input="/home/andrew/namesLevel0"
+group="dev"
+skel="/etc/devskel"
+
+#loop through file, line contents are saved to $line
+while read -r line
+do
+  # store full name frome line.
+  fullname=$line
+  echo $fullname
+
+  # set the values in line to numbered variables 1, 2, 3, etc..
+  set -- $line
+
+  #set a variable to hold first 2 characters in the line
+  firstinital=${1:0:2}
+  #echo $firstinital
+  
+  #set variable to hold the last word of the line
+  lastname="${@: -1}"
+  #echo $lastname
+  
+  #create a username from the first initals and last name, make sure that its all lowercase
+  username="`echo $firstinital$lastname | awk '{print tolower($0)}'`"
+  echo $username
+  
+  # Generate a bad password to go with the username
+  password="2019x$username"
+  
+  # Put the username and bad password in a file together
+  echo $username:$password >> usernamesandpasswords.txt
+  
+  #add user to group
+  useradd -c "$fullname" -g $group -N -m -k $skel -s /bin/tcsh $username
+  #set bad password for user
+  echo -e $username:$password | chpasswd
+  #force user to change it when they log in.
+  chage -d 0 $username
+
+  
+done < "$input" # Where file is specified from input
---
+++ addtemp.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#set directory to get names from, set group to add names to, and specify skel dir.
+input="/home/andrew/namesLevel0"
+group="temp"
+skel="/etc/tempskel"
+
+#loop through file, line contents are saved to $line
+while read -r line
+do
+  # store full name frome line.
+  fullname=$line
+  echo $fullname
+
+  # set the values in line to numbered variables 1, 2, 3, etc..
+  set -- $line
+
+  #set a variable to hold first 2 characters in the line
+  firstinital=${1:0:2}
+  #echo $firstinital
+  
+  #set variable to hold the last word of the line
+  lastname="${@: -1}"
+  #echo $lastname
+  
+  #create a username from the first initals and last name, make sure that its all lowercase
+  username="`echo $firstinital$lastname | awk '{print tolower($0)}'`"
+  echo $username
+  
+  # Generate a bad password to go with the username
+  password="2019x$username"
+  
+  # Put the username and bad password in a file together
+  echo $username:$password >> usernamesandpasswords.txt
+  
+  #add user to group
+  useradd -c "$fullname" -g $group -N -m -k $skel $username
+  #set bad password for user
+  echo -e $username:$password | chpasswd
+  #force user to change it when they log in.
+  chage -d 0 $username
+  #Set quota for temp user
+  setquota -u $username 2048 4096 0 0 /home
+
+  
+done < "$input" # Where file is specified from input
